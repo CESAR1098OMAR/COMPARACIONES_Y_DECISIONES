da8a11722786849d8e7c7448d2d60d8f0aba0356
Cesar Noriega <harten_1098@hotmail.com>
2025-02-22 15:40:20 -0600

    COMPARACIONES Y DECISIONES EN JAVA SCRIPT

---
+++ apps/apps.js
@@ -0,0 +1,65 @@
+let calificacionJuan = 45;
+let calificacionSahara = 85;
+let calificacionCesar = 93;
+let calificacionRoxana = 71;
+let calificacionArchivaldo = -90;
+
+if (calificacionJuan >=0 && calificacionJuan<=100){
+    if(calificacionJuan >= 90 && calificacionJuan <= 100){
+        console.log("Juan tu calificación es de " + calificacionJuan + " puntos. Excelente. Sigue así.")
+    }else if (calificacionJuan >= 75 && calificacionJuan <= 89){
+        console.log("Juan tu calificación es de " + calificacionJuan + " puntos. Bien. Esfurzate un poquito más para ser excelente.")
+    }else if (calificacionJuan >= 60 && calificacionJuan <= 74){
+        console.log("Juan tu calificación es de " + calificacionJuan + " puntos. Suficiente. No te conformes, puedes dar más.")
+    }else {
+        console.log("Juan tu calificación es de " + calificacionJuan + " puntos. Insuficiente, no apruebas.")
+    }
+}else {console.log("Error. La nota debe estar entre 0 y 100, no se admite el valor " + calificacionJuan + ".")}
+
+if (calificacionSahara >=0 && calificacionSahara<=100){
+    if(calificacionSahara >= 90 && calificacionSahara <= 100){
+        console.log("Sahara tu calificación es de " + calificacionSahara + " puntos. Excelente. Sigue así.")
+    }else if (calificacionSahara >= 75 && calificacionSahara <= 89){
+        console.log("Sahara tu calificación es de " + calificacionSahara + " puntos. Bien. Esfurzate un poquito más para ser excelente.")
+    }else if (calificacionSahara >= 60 && calificacionSahara <= 74){
+        console.log("Sahara tu calificación es de " + calificacionSahara + " puntos. Suficiente. No te conformes, puedes dar más.")
+    }else{
+        console.log("Sahara tu calificación es de " + calificacionSahara + " puntos. Insuficiente, no apruebas.")
+    }
+}else{console.log("Error. La nota debe estar entre 0 y 100, no se admite el valor " + calificacionSahara + ".")}
+
+if (calificacionCesar >=0 && calificacionCesar<=100){
+    if(calificacionCesar >= 90 && calificacionCesar <= 100){
+        console.log("César tu calificación es de " + calificacionCesar + " puntos. Excelente. Sigue así.")
+    }else if (calificacionCesar >= 75 && calificacionCesar <= 89){
+        console.log("César tu calificación es de " + calificacionCesar + " puntos. Bien. Esfurzate un poquito más para ser excelente.")
+    }else if (calificacionCesar >= 60 && calificacionCesar <= 74){
+        console.log("César tu calificación es de " + calificacionCesar + " puntos. Suficiente. No te conformes, puedes dar más.")
+    }else{
+        console.log("César tu calificación es de " + calificacionCesar + " puntos. Insuficiente, no apruebas.")
+    }
+}else{console.log("Error. La nota debe estar entre 0 y 100, no se admite el valor " + calificacionCesar + ".")}
+
+if (calificacionRoxana >=0 && calificacionRoxana<=100){
+    if(calificacionRoxana >= 90 && calificacionRoxana <= 100){
+        console.log("Roxana tu calificación es de " + calificacionRoxana + " puntos. Excelente. Sigue así.")
+    }else if (calificacionRoxana >= 75 && calificacionRoxana <= 89){
+        console.log("Roxana tu calificación es de " + calificacionRoxana + " puntos. Bien. Esfurzate un poquito más para ser excelente.")
+    }else if (calificacionRoxana >= 60 && calificacionRoxana <= 74){
+        console.log("Roxana tu calificación es de " + calificacionRoxana + " puntos. Suficiente. No te conformes, puedes dar más.")
+    }else{
+        console.log("Roxana tu calificación es de " + calificacionRoxana + " puntos. Insuficiente, no apruebas.")
+    }
+}else{console.log("Error. La nota debe estar entre 0 y 100, no se admite el valor " + calificacionRoxana + ".")}
+
+if (calificacionArchivaldo >=0 && calificacionArchivaldo<=100){
+    if(calificacionArchivaldo >= 90 && calificacionArchivaldo <= 100){
+        console.log("Archivaldo tu calificación es de " + calificacionArchivaldo + " puntos. Excelente. Sigue así.")
+    }else if (calificacionArchivaldo >= 75 && calificacionArchivaldo <= 89){
+        console.log("Archivaldo tu calificación es de " + calificacionArchivaldo + " puntos. Bien. Esfurzate un poquito más para ser excelente.")
+    }else if (calificacionArchivaldo >= 60 && calificacionArchivaldo <= 74){
+        console.log("Archivaldo tu calificación es de " + calificacionArchivaldo + " puntos. Suficiente. No te conformes, puedes dar más.")
+    }else{
+        console.log("Archivaldo tu calificación es de " + calificacionArchivaldo + " puntos. Insuficiente, no apruebas.")
+    }
+}else{console.log("Error. La nota debe estar entre 0 y 100, no se admite el valor " + calificacionArchivaldo + ".")}
